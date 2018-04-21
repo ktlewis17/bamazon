2a6786d46b8e4de78f70ed6c10901e7c3359bac8
Ktlewis17 <klewis.dpt@gmail.com>
2018-04-20 22:41:56 -0500

    some changes

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer")
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -13,36 +14,59 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("connected as id" + connection.threadId + "\n");
-    queryAllProducts();
+    //console.log("connected as id" + connection.threadId + "\n");
+    AllProducts();
+
+    
 });
 
-function queryAllProducts() {
-    connection.query("SELECT * FROM products", function(err, res) {
+function AllProducts() {
+    connection.query('SELECT * FROM products', function(err, res) {
+      if (err) {console.log(err)};
+
       for (var i = 0; i < res.length; i++) {
-        console.log(res[i].id + " | " + res[i].product_name + " | " + res[i].department_name+ " | " + res[i].price);
-      }
+        console.log(res[i].item_id + " | " + res[i].product_name + " | " + res[i].department_name+ " $" + res[i].price);
+      };
       console.log("-----------------------------------");
+      yourOrder ();
+    
     });
-  }
+  };
 
 
-inquirer.prompt ([
-    { name: "Product ID",
-     message: "What is the product ID you're searching for?",
+function yourOrder () {
+inquirer
+.prompt([
+    { 
+     name: "ID",
+     type: 'input',
+     message: "What is the product ID you're searching for?"
    },
    {
-     name: "Number of units",
-     message:"How many units of the products would you like to purchase?"
-   }
-   ])
- 
-   .then(function(inquirerResponse) {
-     // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
-     if (inquirerResponse.confirm) {
-       console.log("Your selections have been received");
-     }
-     else {
-       console.log("Unavailable" + inquirerResponse)
-     }
-    })
+     name: "Quantity",
+     type: 'input',
+     message: "How many units of the products would you like to purchase?"
+   },
+   ]).then(function(order) {
+      // If the inquirerResponse confirms, we displays the inquirerResponse's username and pokemon from the answers.
+      var selQuantity = order.selQuantity;
+      var itemID = order.ID;
+      var totalPrice = response[0].Price * quantityNeeded;
+        
+    connection.query('SELECT * FROM products WHERE item_id = ' + itemID, function(err, selItem){
+    if (err) {console.log(err) };
+
+    if (selItem[0].stock_Quantity - selQuantity >= 0){
+      var totalPrice = response[0].Price * quantityNeeded;
+      console.log ("Selection successful");
+      console.log("Your total cost for" + selQuantity + " " + response[0].product_name + " is $" + totalPrice + " " );
+
+      connection.query('Update Products SET stock_quantity =? WHERE item_id=?', [selItem[0].stock_Quantity - selQuantity, itemID]);
+    }
+    else {
+      console.log("Insufficient Quantity. Product is unavailable at this time");
+      
+    };
+    AllProducts ();
+  });
+}) }
